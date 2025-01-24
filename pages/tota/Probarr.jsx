47062ab90b29bcd3324f7a8ce12b27fb82a7c8b4
michael <dusacotizaciones@dusa.com.do>
2025-01-24 11:17:36 -0400

    probar

--- pages/tota/Probarr.jsx
+++ pages/tota/Probarr.jsx
@@ -1,7 +1,7 @@
 export default function Probarr({objet,eliminando,editar}){
 
 return <div>
-
+<h2>amgo</h2>
  {objet.map( (a)=>{ return <div key={a.id} >
 <h2>{a.nombre}</h2>
 <button onClick={  function(){ eliminando(a.id )  }  }   >
@@ -11,7 +11,7 @@ return <div>
     editar
 </button>
 
-</div>   }      )}
+</div>   }    )}
  
     <h2>logica</h2>
 </div> 
